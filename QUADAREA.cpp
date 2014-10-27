73d69bbe99ff76522d113adbaa948b1e12e8399a
Sourabh Swain <sourabhswain@gmail.com>
2014-10-27 22:23:51 +0530

    Create QUADAREA.cpp

---
+++ QUADAREA.cpp
@@ -0,0 +1,86 @@
+//Author:Sourabh
+#include<bits/stdc++.h>
+using namespace std;
+//Start template__sunny_red_devil
+typedef long long ll;
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+#define SIZE(X) ((int)(X.size()))
+#define LENGTH(X) ((int)(X.length()))
+#define MP(X,Y) make_pair(X,Y)
+typedef long long LL;
+typedef long long int int64;
+typedef unsigned long long uLL;
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define FU(i,a,n) for(int i=a;i<=n;i++)
+#define FD(i,a,n) for(int i=n;i>=a;i--)
+#define GI(n) scanf("%d",&n)
+#define GD(n) scanf("%lf",&n)
+#define GII(n,m) scanf("%d %d",&n,&m)
+#define PI(n) printf("%d\n",n)
+#define PII(n,m) printf("%d %d\n",n,m)
+#define PL(n) printf("%llu",n)
+#define PLL(n) printf("%lld",n)
+#define GLL(n) scanf("%lld",&n)
+#define GL(n) scanf("%llu",&n)
+#define PN printf("\n")
+//Math functions
+const double pi=acos(-1.0);//pi
+const double eps=1e-11;//eps
+template<class T> inline void checkmin(T &a,T b){if(b<a) a=b;}
+template<class T> inline void checkmax(T &a,T b){if(b>a) a=b;}
+template<class T> inline T sqr(T x){return x*x;}
+template<class T> inline T gcd(T a,T b)
+  {if(a<0)return gcd(-a,b);if(b<0)return gcd(a,-b);return (b==0)?a:gcd(b,a%b);}
+template<class T> inline T lcm(T a,T b)
+  {if(a<0)return lcm(-a,b);if(b<0)return lcm(a,-b);return a*(b/gcd(a,b));}
+//Matrix_functions
+const int MaxMatrixSize=40;//NOTES:MaxMatrixSize
+template<class T> inline void showMatrix(int n,T A[MaxMatrixSize][MaxMatrixSize])//NOTES:showMatrix(
+  {for (int i=0;i<n;i++){for (int j=0;j<n;j++)cout<<A[i][j];cout<<endl;}}
+template<class T> inline T checkMod(T n,T m) {return (n%m+m)%m;}//NOTES:checkMod(
+template<class T> inline void identityMatrix(int n,T A[MaxMatrixSize][MaxMatrixSize])//NOTES:identityMatrix(
+  {for (int i=0;i<n;i++) for (int j=0;j<n;j++) A[i][j]=(i==j)?1:0;}
+template<class T> inline void addMatrix(int n,T C[MaxMatrixSize][MaxMatrixSize],T A[MaxMatrixSize][MaxMatrixSize],T B[MaxMatrixSize][MaxMatrixSize])//NOTES:addMatrix(
+  {for (int i=0;i<n;i++) for (int j=0;j<n;j++) C[i][j]=A[i][j]+B[i][j];}
+template<class T> inline void subMatrix(int n,T C[MaxMatrixSize][MaxMatrixSize],T A[MaxMatrixSize][MaxMatrixSize],T B[MaxMatrixSize][MaxMatrixSize])//NOTES:subMatrix(
+  {for (int i=0;i<n;i++) for (int j=0;j<n;j++) C[i][j]=A[i][j]-B[i][j];}
+template<class T> inline void mulMatrix(int n,T C[MaxMatrixSize][MaxMatrixSize],T _A[MaxMatrixSize][MaxMatrixSize],T _B[MaxMatrixSize][MaxMatrixSize])//NOTES:mulMatrix(
+  { T A[MaxMatrixSize][MaxMatrixSize],B[MaxMatrixSize][MaxMatrixSize];
+  for (int i=0;i<n;i++) for (int j=0;j<n;j++) A[i][j]=_A[i][j],B[i][j]=_B[i][j],C[i][j]=0;
+  for (int i=0;i<n;i++) for (int j=0;j<n;j++) for (int k=0;k<n;k++) C[i][j]+=A[i][k]*B[k][j];}
+//Point&Line
+double dist(double x1,double y1,double x2,double y2){return sqrt(sqr(x1-x2)+sqr(y1-y2));}//NOTES:dist(
+double distR(double x1,double y1,double x2,double y2){return sqr(x1-x2)+sqr(y1-y2);}//NOTES:distR(
+template<class T> T cross(T x0,T y0,T x1,T y1,T x2,T y2){return (x1-x0)*(y2-y0)-(x2-x0)*(y1-y0);}//NOTES:cross(
+int crossOper(double x0,double y0,double x1,double y1,double x2,double y2)//NOTES:crossOper(
+  {double t=(x1-x0)*(y2-y0)-(x2-x0)*(y1-y0);if (fabs(t)<=eps) return 0;return (t<0)?-1:1;}
+bool isIntersect(double x1,double y1,double x2,double y2,double x3,double y3,double x4,double y4)//NOTES:isIntersect(
+  {return crossOper(x1,y1,x2,y2,x3,y3)*crossOper(x1,y1,x2,y2,x4,y4)<0 && crossOper(x3,y3,x4,y4,x1,y1)*crossOper(x3,y3,x4,y4,x2,y2)<0;}
+bool isMiddle(double s,double m,double t){return fabs(s-m)<=eps || fabs(t-m)<=eps || (s<m)!=(t<m);}//NOTES:isMiddle(  
+//Translator
+bool isUpperCase(char c){return c>='A' && c<='Z';}//NOTES:isUpperCase(
+bool isLowerCase(char c){return c>='a' && c<='z';}//NOTES:isLowerCase(
+bool isLetter(char c){return c>='A' && c<='Z' || c>='a' && c<='z';}//NOTES:isLetter(
+bool isDigit(char c){return c>='0' && c<='9';}//NOTES:isDigit(
+char toLowerCase(char c){return (isUpperCase(c))?(c+32):c;}//NOTES:toLowerCase(
+char toUpperCase(char c){return (isLowerCase(c))?(c-32):c;}//NOTES:toUpperCase(
+template<class T> string toString(T n){ostringstream ost;ost<<n;ost.flush();return ost.str();}//NOTES:toString(
+int toInt(string s){int r=0;istringstream sin(s);sin>>r;return r;}//NOTES:toInt(
+int64 toInt64(string s){int64 r=0;istringstream sin(s);sin>>r;return r;}//NOTES:toInt64(
+double toDouble(string s){double r=0;istringstream sin(s);sin>>r;return r;}//NOTES:toDouble(
+//End template__sunny_red_devil
+double a,b,c,d,s;
+
+int main() {
+	
+	int t;
+	GI(t);
+	while(t--){
+		scanf("%lf %lf %lf %lf",&a,&b,&c,&d);
+		s = (a+b+c+d)/2;
+		double area = sqrt((s-a)*(s-b)*(s-c)*(s-d));
+		printf("%0.2lf\n",area);
+	}
+	return 0;
+}
